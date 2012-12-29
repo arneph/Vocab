a5f16849b36bdcf3258c6905e015acd9a369e551
Arne Philipeit <arnphi97@googlemail.com>
2012-12-29 20:35:02 +0100

    Initial Commit

---
+++ Vocab/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Vocab
+//
+//  Created by Programmieren on 29.12.12.
+//  Copyright (c) 2012 Arne Philipeit Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Vocab/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Vocab
+//
+//  Created by Programmieren on 29.12.12.
+//  Copyright (c) 2012 Arne Philipeit Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
